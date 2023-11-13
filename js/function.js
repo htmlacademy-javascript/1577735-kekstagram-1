e10db22abbf534603caebce1da2b4be2b9e8244f
Aleksandr Shishkov <kolobok.87@bk.ru>
2023-11-13 20:43:32 +0300

    добавляет генерацию данных

--- js/function.js
+++ js/function.js
@@ -41,7 +41,7 @@ extractNumber(1.5);
 
 //Функция, которая принимает три параметра: исходную строку, минимальную длину и строку с добавочными символами — и возвращает исходную строку, дополненную указанными символами до заданной длины.
 function myPadStart(string,minLength,pad){
-  const actualPad = minLength - string.lenth;
+  const actualPad = minLength - string.length;
   if(actualPad <= 0){
     return string;
   }
